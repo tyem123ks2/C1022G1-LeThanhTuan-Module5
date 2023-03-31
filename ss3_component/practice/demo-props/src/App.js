5de97b537f49e7a5c076a8aa0475773ee6b261b2
tyem123ks2 <thanhtuan02051011@gmail.com>
2023-03-31 09:39:50 +0700

    first commit

---
+++ ss3_component/practice/demo-props/src/App.js
@@ -0,0 +1,10 @@
+
+    import AddComponent from "./components/AddComponent";
+
+    function App() {
+        return (
+            <AddComponent name={'Nguyễn Văn A'} age = {'14'} address = {'Quảng Trị'} />
+        )
+    }
+
+    export default App;
---
+++ ss3_component/practice/demo-props/src/components/AddComponent.js
@@ -0,0 +1,20 @@
+function AddComponent(props) {
+    return (
+        <table>
+        <tr>
+            <td>ID</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Address</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>{props.name}</td>
+            <td>{props.age}</td>
+            <td>{props.address}</td>
+        </tr>
+        </table>
+    );
+}
+
+export default AddComponent;
