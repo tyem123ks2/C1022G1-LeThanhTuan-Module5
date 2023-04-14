acb7c2f657e4891dce198ab06f4ddb60278e31e2
tyem123ks2 <thanhtuan02051011@gmail.com>
2023-04-14 14:21:23 +0700

    first commit

--- Thi/FE-Repository/exam_module5/src/service/bookService.js
+++ Thi/FE-Repository/exam_module5/src/service/bookService.js
@@ -6,16 +6,16 @@ export const getAllBooks = async () => {
 }
 
 export const getBookId = async (id) => {
-    return (await axios.get(`http://localhost:3003/books/${id}`)).data
+    return (await axios.get(`http://localhost:8080/books/${id}`)).data
 };
 
 export const createBooks = async (books) => {
-    return await axios.post(`http://localhost:3003/books`, {...books});
+    return await axios.post(`http://localhost:8080/books`, {...books});
 };
 
 export const remove = async (id) => {
     try {
-        await axios.delete(`http://localhost:3003/books/${id}`)
+        await axios.delete(`http://localhost:8080/books/${id}`)
         alert("Xóa sách thành công!!")
     } catch (e) {
         console.log(e)
